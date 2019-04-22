434de80f03a7f8068e15438b52488acacb50ab06
billsnow12 <hiimwillcho@gmail.com>
2019-04-22 16:43:00 -0400

    button logic

--- barterApp/barterApp/Controller/Listings/ListingsViewController.swift
+++ barterApp/barterApp/Controller/Listings/ListingsViewController.swift
@@ -10,10 +10,83 @@ import UIKit
 import Firebase
 import GoogleSignIn
 
-class ListingsViewController: UIViewController {
+struct Cell{
+    let image: UIImage?
+    let title: String?
+}
 
+class ListingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var ref: DatabaseReference?
+    var databaseHandle: DatabaseHandle?
+    var postData = [String]()
+    var data = [Cell]()
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
+        let index = postData.count - indexPath.row - 1
+        cell.textLabel?.text = postData[index]
+        return cell
+        
+    }
+    
+    @IBOutlet weak var favoritesOutlet: UIButton!
+    @IBOutlet weak var tradingOutlet: UIButton!
+    @IBOutlet weak var tradedOutlet: UIButton!
+    
+    
+    @IBAction func favoritesButton(_ sender: Any) {
+        enableAllButtons()
+        favoritesOutlet.isEnabled = false
+    }
+    
+    @IBAction func tradingButton(_ sender: Any) {
+        enableAllButtons()
+        tradingOutlet.isEnabled = false
+    }
+    
+    @IBAction func tradedButton(_ sender: Any) {
+        enableAllButtons()
+        tradedOutlet.isEnabled = false
+    }
+    
+    func enableAllButtons(){
+        favoritesOutlet.isEnabled = true
+        tradingOutlet.isEnabled = true
+        tradedOutlet.isEnabled = true
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        ref = Database.database().reference()
+        
+        databaseHandle = ref?.child("barters").observe(.childAdded, with: { (DataSnapshot) in
+            let post = DataSnapshot.value as? String
+            
+            if let actualPost = post {
+                self.postData.append(actualPost)
+                self.tableView.reloadData()
+            }
+        })
+        
+        
+        favoritesButton(self)
+        
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
