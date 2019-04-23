369cd2f63e05bfbf6ad18882709fddb87871ee35
Kevin Maldjian <kevinmaldjian@gmail.com>
2019-04-23 10:53:48 -0400

    adding apple mail functionality

--- barterApp/barterApp/Controller/Feed/ItemInfoViewController.swift
+++ barterApp/barterApp/Controller/Feed/ItemInfoViewController.swift
@@ -106,16 +106,19 @@ class ItemInfoViewController: UIViewController {
             }
         }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func sendEmail(_ sender: Any) {
+        let email = seller.email!
+        if let url = URL(string: "mailto:\(email)") {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+        //print(seller.email)
     }
-    */
-
+    
 }
 
 
