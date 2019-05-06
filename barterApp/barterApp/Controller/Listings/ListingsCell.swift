1770fc7193f04f9754bda294e9145cbd22dc6dec
billsnow12 <hiimwillcho@gmail.com>
2019-05-06 13:23:20 -0400

    add custom cell

---
+++ barterApp/barterApp/Controller/Listings/ListingsCell.swift
@@ -0,0 +1,20 @@
+//
+//  ListingsCell.swift
+//  barterApp
+//
+//  Created by Sungjea Cho on 2019-05-06.
+//  Copyright © 2019 Kevin Maldjian. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListingsCell: UITableViewCell{
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var descr: UILabel!
+    
+    @IBOutlet weak var imagee: UIImageView!
+    
+    
+}
