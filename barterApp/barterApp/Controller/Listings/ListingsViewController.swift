2fc68d35f2353eb8261503fef29b42172008aa0f
Kevin Maldjian <kevinmaldjian@gmail.com>
2019-05-06 20:12:27 -0400

    removing redundant code

--- barterApp/barterApp/Controller/Listings/ListingsViewController.swift
+++ barterApp/barterApp/Controller/Listings/ListingsViewController.swift
@@ -158,32 +158,5 @@ class ListingsViewController: UIViewController, UITableViewDelegate, UITableView
     
     
     
-    
-    //-------
 
-    
-
-    @IBAction func logOut(_ sender: Any) {
-        print("Bye")
-        do {
-            try Auth.auth().signOut()
-            if GIDSignIn.sharedInstance().hasAuthInKeychain() {
-                GIDSignIn.sharedInstance().signOut()
-                print("Signing out Google User")
-            }
-        } catch let logoutError {
-            print(logoutError)
-        }
-        goFirstScreen()
-    }
-    
-    func goFirstScreen() {
-        self.view.window!.rootViewController?.dismiss(animated: false, completion: nil)
-        //self.navigationController?.popToRootViewController(animated: true)
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let mainPage = storyboard.instantiateViewController(withIdentifier: "MenuViewController")
-        self.present(mainPage, animated: true, completion: nil)
-        InterfaceManager.sharedInstance.mainTabViewCon = nil
-        BACurrentUser.currentUser.uid = nil
-    }
 }
